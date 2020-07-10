119cb39270086539ec89448fbb754eeda3e8d261
MellowCo <799478052@qq.com>
2020-07-10 11:28:19 +0800

    检查登录

--- src/utils/request/request.js
+++ src/utils/request/request.js
@@ -1,4 +1,15 @@
-export default function ({ url, data, method, baseUrl = process.env.VUE_APP_BASE_URL, loading = true }) {
+import { isLogin, toastText } from '../wxUtils/index'
+
+export default function ({ url, data, method, baseUrl = process.env.VUE_APP_BASE_URL, loading = true, checkLogin = false }) {
+  if (checkLogin && isLogin()) {
+    uni.switchTab({
+      url: '/pages/settings/index'
+    })
+
+    toastText({ title: '请先登录' })
+    return
+  }
+
   if (loading) {
     uni.showLoading({
       title: '加载中'
